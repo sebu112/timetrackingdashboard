164a7cae6799c600930087e0d1b266f9ac9f85e3
Sebastian Laukkanen <sabu.laukkanen@gmail.com>
2022-09-06 12:50:42 +0300

    Kommitin tynkää

---
+++ script.js
@@ -0,0 +1,46 @@
+'use script'
+
+function daily () {
+    document.getElementById("työ").innerHTML=("5hrs");
+    document.getElementById("työ1").innerHTML=("Yesterday - 7hrs");
+    document.getElementById("pelaaminen").innerHTML=("1hrs");
+    document.getElementById("pelaaminen1").innerHTML=("Yesterday - 2hrs");
+    document.getElementById("opiskelu").innerHTML=("0hrs");
+    document.getElementById("opiskelu1").innerHTML=("Yesterday - 1hrs");
+    document.getElementById("urheilu").innerHTML=("1hrs");
+    document.getElementById("urheilu1").innerHTML=("yesterday - 1hrs");
+    document.getElementById("some").innerHTML=("1hrs");
+    document.getElementById("some1").innerHTML=("Yesterday - 3hrs");
+    document.getElementById("huolto").innerHTML=("0hrs");
+    document.getElementById("huolto1").innerHTML=("Yesterday - 1hrs");
+}
+
+function weekly () {
+   document.getElementById("työ").innerHTML=("32hrs");
+   document.getElementById("työ1").innerHTML=("Last week - 36hrs");
+   document.getElementById("pelaaminen").innerHTML=("10hrs");
+   document.getElementById("pelaaminen1").innerHTML=("Last week - 8hrs");
+   document.getElementById("opiskelu").innerHTML=("4hrs");
+   document.getElementById("opiskelu1").innerHTML=("Last week - 7hrs");
+   document.getElementById("urheilu").innerHTML=("4hrs");
+   document.getElementById("urheilu1").innerHTML=("Last week - 5hrs");
+   document.getElementById("some").innerHTML=("5hrs");
+   document.getElementById("some1").innerHTML=("Last week - 10hrs");
+   document.getElementById("huolto").innerHTML=("2hrs");
+   document.getElementById("huolto1").innerHTML=("Last week - 2hrs");
+}
+
+function monthly () {
+    document.getElementById("työ").innerHTML=("103");
+    document.getElementById("työ1").innerHTML=("Last month - 128hrs");
+    document.getElementById("pelaaminen").innerHTML=("23hrs");
+    document.getElementById("pelaaminen1").innerHTML=("Last month - 29hrs");
+    document.getElementById("opiskelu").innerHTML=("13hrs");
+    document.getElementById("opiskelu1").innerHTML=("Last month - 19hrs");
+    document.getElementById("urheilu").innerHTML=("11hrs");
+    document.getElementById("urheilu1").innerHTML=("Last month - 18hrs");
+    document.getElementById("some").innerHTML=("21");
+    document.getElementById("some1").innerHTML=("Last month - 23hrs");
+    document.getElementById("huolto").innerHTML=("7hrs");
+    document.getElementById("huolto1").innerHTML=("Last month - 11hrs");
+}
